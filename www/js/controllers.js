c8d1a0e291275a30846520ba6a6984a0467432f7
eckelon <eckelon@gmail.com>
2015-07-12 17:24:21 +0200

    remove debug message

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,9 +4,7 @@ angular.module('starter.controllers', [])
     var contaminantsChart = new ContaminantsChart({'url':'http://zgzvizdata.herokuapp.com/data'});
 })
 .controller('EstacionesCtrl', function($scope) {
-    alert("foo");
 var stationsMap = new StationsMap({'url':'http://zgzvizdata.herokuapp.com/stations_coordinates'});
-    alert("bar");
 })
 
 .controller('AcercadeCtrl', function($scope) {});
